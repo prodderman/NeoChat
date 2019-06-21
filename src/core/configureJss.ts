bb3497a2fd4caea64424e8009dd59255227639a2
Stepan <ranibowprodder@gmail.com>
2019-06-22 02:21:57 +0700

    fix configureJss

--- src/core/configureJss.ts
+++ src/core/configureJss.ts
@@ -1,11 +1,9 @@
-import { create } from 'jss';
+import { create, createGenerateClassName } from 'jss';
 import jssCompose from 'jss-compose';
-import { createGenerateClassName, jssPreset } from '@material-ui/core/styles';
 import { IJssDependencies } from 'shared/types/app';
 
 export function configureJss(virtual?: boolean): IJssDependencies {
-  // Place to add jss-plugins [https://material-ui.com/customization/css-in-js/#plugins]
-  const jss = create({ virtual, plugins: [...jssPreset().plugins, jssCompose()] });
+  const jss = create({ virtual, plugins: [jssCompose()] });
   jss.setup({ insertionPoint: 'jss-insertion-point' });
   const generateClassName = createGenerateClassName();
 
